73531e6a0e85d66d959d41db6a3cf239428c91c4
jaimin patel <jaiminp2k6@gmail.com>
2025-09-02 22:08:44 +0530

    updete the code

--- jumblewords.py
+++ jumblewords.py
@@ -1,10 +1,9 @@
 import random
 import nltk
 
-
+# Download words corpus if not already present
 nltk.download('words')
 from nltk.corpus import words
-
 
 word_list = words.words()
 
@@ -15,52 +14,54 @@ def jumble(word):
     return " ".join(random.sample(word, len(word)))
 
 def thank(p1n, p2n, p1, p2):
+    print("\nFinal Scores:")
     print(p1n, 'Your score is:', p1)
     print(p2n, 'Your score is:', p2)
-    print('Thanks for playing')
-    print('Have a nice day')
+
+    # Winner announcement
+    if p1 > p2:
+        print(f"🎉 {p1n} wins the game!")
+    elif p2 > p1:
+        print(f"🎉 {p2n} wins the game!")
+    else:
+        print("🤝 It's a tie!")
+
+    print('Thanks for playing!')
+    print('Have a nice day 😊')
 
 def play():
     p1name = input('Player 1, please enter your name: ')
     p2name = input('Player 2, please enter your name: ')
     pp1 = 0 
     pp2 = 0
-    turn = 0
 
     while True:
         picked_word = choose()    
         qn = jumble(picked_word)
         print("\nJumbled word:", qn)
-        
-        if turn % 2 == 0:
-            print(p1name, 'your turn.')
-            ans = input('What is on my mind? ')
-            
-            if ans == picked_word:
-                pp1 += 1
-                print('Correct! Your score is:', pp1)
-            else:
-                print('Better luck next time, I thought:', picked_word)
 
-            c = input('Press 1 to continue and 0 to quit: ')
-            if c == '0':
-                thank(p1name, p2name, pp1, pp2)
-                break
+        # Player 1 turn
+        print(p1name, 'your turn.')
+        ans1 = input('What is on my mind? ')
+        if ans1 == picked_word:
+            pp1 += 1
+            print('✅ Correct! Your score is:', pp1)
         else:
+            print('❌ Wrong answer! Let’s see if', p2name, 'can guess...')
+
+            # Player 2 turn only if Player 1 is wrong
             print(p2name, 'your turn.')
-            ans = input('What is on my mind? ')
-            
-            if ans == picked_word:
+            ans2 = input('What is on my mind? ')
+            if ans2 == picked_word:
                 pp2 += 1
-                print('Correct! Your score is:', pp2)
+                print('✅ Correct! Your score is:', pp2)
             else:
-                print('Better luck next time, I thought:', picked_word)
+                print('❌ Wrong again! The correct word was:', picked_word)
 
-            c = input('Press 1 to continue and 0 to quit: ')
-            if c == '0':
-                thank(p1name, p2name, pp1, pp2)
-                break  
-
-        turn += 1
+        # Continue or quit
+        c = input('\nPress 1 to continue and 0 to quit: ')
+        if c == '0':
+            thank(p1name, p2name, pp1, pp2)
+            break
 
 play()
